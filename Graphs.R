4bcff8e0528cd0ba6b662c8033d1d5a4945457f1
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-06-17 07:20:36 +0200

    Használható barplot példa hozzáadva
    Keresgélés után találtam egy ggplotos példát, ami jónak tűnik.

--- Graphs.R
+++ Graphs.R
@@ -13,3 +13,26 @@ sumnov.df <- cbind(sumnov.df, as.data.frame(summary(full.nov[,1]))) # Hely szám
 names(sumnov.df) <- c("Mean", "Count")
 
 sumall.df <- rbind(sum.df, sumnov.df)
+
+## Variable column width
+## https://www.r-graph-gallery.com/81-barplot-with-variable-width.html
+
+library(ggplot2)
+
+# make data
+data <- data.frame(
+  group=c("A ","B ","C ","D ") ,
+  value=c(33,62,56,67) ,
+  number_of_obs=c(100,500,459,342)
+)
+
+# Calculate the future positions on the x axis of each bar (left border, central position, right border)
+data$right <- cumsum(data$number_of_obs) + 30*c(0:(nrow(data)-1))
+data$left <- data$right - data$number_of_obs
+
+# Plot
+ggplot(data, aes(ymin = 0)) +
+    geom_rect(aes(xmin = left, xmax = right, ymax = value, colour = group, fill = group)) +
+    xlab("number of obs") +
+    ylab("value") +
+    theme(legend.position="none")
