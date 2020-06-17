c708f7b0648b58f6bdd6a06fa990ec2257d2ee73
Péter Kalicz <kalicz.peter@uni-sopron.hu>
2020-06-17 07:04:00 +0200

    Ábrakészítés hozzáadva
    Boxplot hozzáadva, illetve az oszlopdiagram előkészítése a beolvasásból.

---
+++ Graphs.R
@@ -0,0 +1,14 @@
+boxplot(Diff ~ Kat, full.df) # Boxplot
+
+full.pos <- full.df[full.df$Diff <= 0, c("Kat", "Diff")] # Hol csökkent?
+
+sum.df <- as.data.frame(tapply(full.pos[,2], full.pos[,1], mean)) # Átlagos csökkenés
+sum.df <- cbind(sum.df, as.data.frame(summary(full.pos[,1]))) # Hely szám
+names(sum.df) <- c("Mean", "Count")
+
+full.nov <- full.df[full.df$Diff > 0, c("Kat", "Diff")] # Hol nőtt?
+sumnov.df <- as.data.frame(tapply(full.nov[,2], full.nov[,1], mean)) # Átlagos növekedés
+sumnov.df <- cbind(sumnov.df, as.data.frame(summary(full.nov[,1]))) # Hely szám
+names(sumnov.df) <- c("Mean", "Count")
+
+sumall.df <- rbind(sum.df, sumnov.df)
